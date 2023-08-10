c4d9c879cc28f2ce2e9bc54c1ae0eddcb63c3d9e
gonplz <gonzaplazaola@gmai.com>
2023-08-10 18:08:23 -0300

    Detallando la Task3

---
+++ src/main/resources/static/web/js/account.js
@@ -0,0 +1,33 @@
+Vue.createApp({
+    data() {
+        return {
+            accountInfo: {},
+            errorToats: null,
+            errorMsg: null,
+        }
+    },
+    methods: {
+        getData() {
+            const urlParams = new URLSearchParams(window.location.search);
+            const id = urlParams.get('id');
+            axios.get(`/api/accounts/${id}`)
+                .then((response) => {
+                    //get client ifo
+                    this.accountInfo = response.data;
+                    this.accountInfo.transactions.sort((a, b) => b.id - a.id)
+                })
+                .catch((error) => {
+                    // handle error
+                    this.errorMsg = "Error getting data";
+                    this.errorToats.show();
+                })
+        },
+        formatDate(date) {
+            return new Date(date).toLocaleDateString('en-gb');
+        }
+    },
+    mounted() {
+        this.errorToats = new bootstrap.Toast(document.getElementById('danger-toast'));
+        this.getData();
+    }
+}).mount('#app');
